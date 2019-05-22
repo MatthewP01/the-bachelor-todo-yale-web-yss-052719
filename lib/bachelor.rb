3e66654979cc303d3f895d350cb35d2b4ee61c22
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-22 21:51:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,6 +45,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
+  age_array = []
   data[season].each do |contestant|
     age = contestant["age"].mean
   end
