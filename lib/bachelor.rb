af0f2001e4052ac6b31f49e875ecd292cfa7b5f2
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-22 22:00:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,6 +52,6 @@ def get_average_age_for_season(data, season)
 
   end
   return age_array
-  age_array.inject(0) {|sum, i|  sum + i } / age_array.size.round
+  mean = age_array.inject(0) {|sum, i|  sum + i } / age_array.size.round
 
 end
