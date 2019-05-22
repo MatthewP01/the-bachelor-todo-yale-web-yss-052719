759a4b19af07523361964d285c01c32d8933e011
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-22 21:46:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,6 +37,7 @@ def get_occupation(data, hometown)
     contestant_data.each do |selection|
       selection.each do |key, value|
         if value == hometown
+          
         end
       end
     end
