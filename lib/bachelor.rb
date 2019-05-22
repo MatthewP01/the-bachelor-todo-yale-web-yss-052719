983b106e1015fa9bcf097a6325d0e7e3660630c0
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-22 21:12:59 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |winner|
     if winner["status"] == "winner"
-      puts ""
+      return
 end
 
 def get_contestant_name(data, occupation)
